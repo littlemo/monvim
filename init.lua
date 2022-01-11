0134ed573a759cd3561fca0cd6683f6a09a2e6b7
Moore.Huang <moore@moorehy.com>
2022-01-11 16:33:36 +0800

    Optimize: 优化 python & markdown 文件下的显示模式定义，增加配置折叠方法为缩进

--- init.lua
+++ init.lua
@@ -132,8 +132,8 @@ hooks.add("install_plugins", function(use)
     -- 文件类型检测
     "nathom/filetype.nvim",
     config = function()
-      vim.cmd [[ autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
-      vim.cmd [[ autocmd Filetype markdown setlocal noexpandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
+      vim.cmd [[ autocmd Filetype python setlocal foldmethod=indent expandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
+      vim.cmd [[ autocmd Filetype markdown setlocal foldmethod=indent noexpandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
     end,
   } -- }}}
     -- }}}
