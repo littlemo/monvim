01cb1e628ae4ad20698ef8c6444b146c232691cd
Moore.Huang <moore@moorehy.com>
2022-01-13 12:06:34 +0800

    Config: 配置各常用语言的显示模式

--- init.lua
+++ init.lua
@@ -127,9 +127,10 @@ hooks.add("install_plugins", function(use)
     -- 文件类型检测
     "nathom/filetype.nvim",
     config = function()
-      vim.cmd [[ autocmd Filetype python setlocal foldmethod=indent expandtab tabstop=4 shiftwidth=4 softtabstop=4 nofoldenable ]]
-      vim.cmd [[ autocmd Filetype markdown setlocal foldmethod=indent noexpandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
+      vim.cmd [[ autocmd Filetype python setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr() expandtab tabstop=4 shiftwidth=4 softtabstop=4 nofoldenable ]]
+      vim.cmd [[ autocmd Filetype markdown setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr() noexpandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
       vim.cmd [[ autocmd Filetype lua setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr() expandtab tabstop=2 shiftwidth=2 softtabstop=2 ]]
+      vim.cmd [[ autocmd Filetype json setlocal foldmethod=indent expandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
     end,
   }
   -- 分屏&窗口
