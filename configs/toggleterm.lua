9621d85a90439f9735dc7f6d41f3832519b90377
Moore.Huang <moore@moorehy.com>
2022-02-05 16:54:47 +0800

    Optimize: 优化 toggleterm 的终端切换快键映射

--- configs/toggleterm.lua
+++ configs/toggleterm.lua
@@ -28,12 +28,23 @@ M.setup = function()
     close_on_exit = true, -- close the terminal window when the process exits
     shell = vim.o.shell, -- change the default shell
   })
+  function _set_terminal_keymaps()
+    local opts = {noremap = true}
+    vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, 't', '<C-w><C-h>', [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, 't', '<C-w><C-j>', [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, 't', '<C-w><C-k>', [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, 't', '<C-w><C-l>', [[<C-\><C-n><C-W>l]], opts)
+  end
+  -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+  vim.cmd [[ autocmd! TermOpen term://* lua _set_terminal_keymaps() ]]
 end
 
 M.config = function()
   local Terminal  = require('toggleterm.terminal').Terminal
   local lazygit = Terminal:new({
     cmd = "lazygit",
+    count = 5,
     dir = "git_dir",
     direction = "float",
     close_on_exit = true,
@@ -43,7 +54,7 @@ M.config = function()
     -- function to run on opening the terminal
     on_open = function(term)
       vim.cmd("startinsert!")
-      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = false})
     end,
     -- function to run on closing the terminal
     -- on_close = function(term)
@@ -56,7 +67,7 @@ M.config = function()
   end
 
   -- vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
-  vim.cmd [[ command! -count=1 ToggleLazygit lua _lazygit_toggle() ]]
+  vim.cmd [[ command! -count=5 ToggleLazygit lua _lazygit_toggle() ]]
 end
 
 return M
