66708deaee2968b3e15992181a068617c897cf23
Moore.Huang <moore@moorehy.com>
2022-02-05 13:49:03 +0800

    Feature: 添加 revJ 插件，用于拆分换行函数参数列表

--- plugins.lua
+++ plugins.lua
@@ -126,6 +126,29 @@ return {
       require("custom.configs.dial").config()
     end,
   },
+  {'kana/vim-textobj-user', event = 'BufRead'},
+  {'sgur/vim-textobj-parameter', event = 'BufRead', after = 'vim-textobj-user'},
+  { -- nvim-revJ.lua
+    -- 支持行拆分功能，与 join-line(J) 相反
+    'AckslD/nvim-revJ.lua',
+    event = 'BufRead',
+    after = {'vim-textobj-user', 'vim-textobj-parameter'},
+    config = function()
+      require("revj").setup {
+          brackets = {first = '([{<', last = ')]}>'}, -- brackets to consider surrounding arguments
+          new_line_before_last_bracket = true, -- add new line between last argument and last bracket (only if no last seperator)
+          add_seperator_for_last_parameter = true, -- if a seperator should be added if not present after last parameter
+          enable_default_keymaps = false, -- enables default keymaps without having to set them below
+          keymaps = {
+              operator = '<localleader>j', -- for operator (+motion)
+              line = '<localleader>J', -- for formatting current line
+              visual = '<localleader>J', -- for formatting visual selection
+          },
+          parameter_mapping = ',', -- specifies what text object selects an arguments (ie a, and i, by default)
+              -- if you're using `vim-textobj-parameter` you can also set this to `vim.g.vim_textobj_parameter_mapping`
+      }
+    end,
+  },
   -- 光标线
   { -- nvim-cursorword
     "xiyaowong/nvim-cursorword",
