f884b2b3de10fdf3d955576930d91da1af09dfcf
Moore.Huang <moore@moorehy.com>
2022-02-06 14:44:24 +0800

    Optimize: 配置 lspconfig 语言服务

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,24 +3,22 @@ local M = {}
 M.setup_lsp = function(attach, capabilities)
   local lspconfig = require "lspconfig"
 
-  -- lspservers with default config
-
-  -- lspconfig.pyright.setup {
-  --   on_attach = attach,
-  --   capabilities = capabilities,
-  --   flags = {
-  --     debounce_text_changes = 150,
-  --   },
-  -- }
-
-  lspconfig.jedi_language_server.setup {
-    on_attach = attach,
-    capabilities = capabilities,
-    single_file_support = true,
-    flags = {
-      debounce_text_changes = 150,
-    },
+  local servers = {
+    "sumneko_lua",  -- 安装 brew install lua-language-server
+    "jedi_language_server",  -- 安装 pip3 install -U jedi-language-server
+    "pyright",  -- 安装 pip3 install pyright / npm install -g pyright
   }
+
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+      on_attach = attach,
+      capabilities = capabilities,
+      single_file_support = true,
+      flags = {
+        debounce_text_changes = 150,
+      },
+    }
+  end
 end
 
 return M
