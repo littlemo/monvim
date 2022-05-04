b8dd71e19575c48bab019668c23c84335d7d6a9b
Moore.Huang <moore@moorehy.com>
2022-05-04 22:28:58 +0800

    Config: 添加常见功能的 autocmd 定义开启

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -32,22 +32,22 @@ local autocmd = vim.api.nvim_create_autocmd
 -- })
 
 -- File extension specific tabbing
--- autocmd("Filetype", {
---    pattern = "python",
---    callback = function()
---       vim.opt_local.expandtab = true
---       vim.opt_local.tabstop = 4
---       vim.opt_local.shiftwidth = 4
---       vim.opt_local.softtabstop = 4
---    end,
--- })
+autocmd("Filetype", {
+   pattern = "python",
+   callback = function()
+      vim.opt_local.expandtab = true
+      vim.opt_local.tabstop = 4
+      vim.opt_local.shiftwidth = 4
+      vim.opt_local.softtabstop = 4
+   end,
+})
 
 -- Highlight yanked text
--- autocmd("TextYankPost", {
---    callback = function()
---       vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
---    end,
--- })
+autocmd("TextYankPost", {
+   callback = function()
+      vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+   end,
+})
 
 -- Enable spellchecking in markdown, text and gitcommit files
 -- autocmd("FileType", {
