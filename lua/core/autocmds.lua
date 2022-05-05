9d5182c22213c08d6c09b9fbe8b34de674610067
Moore.Huang <moore@moorehy.com>
2022-05-05 22:22:43 +0800

    config(autocmds): 补充 markdown / json / yaml 文件的缩进&折叠配置

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -42,6 +42,42 @@ autocmd("Filetype", {
    end,
 })
 
+autocmd("Filetype", {
+   pattern = "markdown",
+   callback = function()
+      vim.opt_local.foldmethod = "indent"
+      vim.opt_local.expandtab = true
+      vim.opt_local.tabstop = 4
+      vim.opt_local.shiftwidth = 4
+      vim.opt_local.softtabstop = 4
+      vim.opt_local.foldlevel = 1
+   end,
+})
+
+autocmd("Filetype", {
+   pattern = "json",
+   callback = function()
+      vim.opt_local.foldmethod = "indent"
+      vim.opt_local.expandtab = true
+      vim.opt_local.tabstop = 4
+      vim.opt_local.shiftwidth = 4
+      vim.opt_local.softtabstop = 4
+      vim.opt_local.foldlevel = 1
+   end,
+})
+
+autocmd("Filetype", {
+   pattern = "yaml",
+   callback = function()
+      vim.opt_local.foldmethod = "indent"
+      vim.opt_local.expandtab = true
+      vim.opt_local.tabstop = 2
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.softtabstop = 2
+      vim.opt_local.foldlevel = 1
+   end,
+})
+
 -- Highlight yanked text
 autocmd("TextYankPost", {
    callback = function()
