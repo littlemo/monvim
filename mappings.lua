b342aa7e0b6e9b6cbc389c8acd3be0783ea6d3a1
Moore.Huang <moore@moorehy.com>
2022-05-30 13:17:11 +0800

    feat(mapping): 添加通用基础功能的按键映射定义

--- mappings.lua
+++ mappings.lua
@@ -11,10 +11,20 @@ M.disabled = {
 }
 
 M.general = {
+  n = {
+    ["q"] = { ":q<CR>", "  quit" },
+    ["Q"] = { ":qa<CR>", "  quit all" },
+    ["<localleader>q"] = { "q", "壘  record" },
+    ["<localleader>Q"] = { "@q", "奈  play record" },
+  },
   i = {
     ["<C-f>"] = { "<Right>", "  forward character" },
     ["<C-b>"] = { "<Left>", "  backward character" },
     ["<C-a>"] = { "<ESC>^i", "論 beginning of line" },
+  },
+  x = {
+    ["<"] = { "<gv", "  左缩进" },
+    [">"] = { ">gv", "  右缩进" },
   },
 }
 
