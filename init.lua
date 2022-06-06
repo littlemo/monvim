e5a41807a7fc6d28c40f462aa6c5fab2b8409a8e
Moore.Huang <moore@moorehy.com>
2022-06-06 12:09:52 +0800

    feat(command): 配置全局 json 格式化命令

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 -- example file i.e lua/custom/init.lua
 
 -- load your globals, autocmds here or anything .__.
+-- json 格式化
+vim.cmd [[ com! FormatJSON %!python3 -c "import json, sys, collections; print(json.dumps(json.load(sys.stdin, object_pairs_hook=collections.OrderedDict), ensure_ascii=False, indent=4))" ]]
