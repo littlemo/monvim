65a42d26565051f17d2cc468a6221c81d6a2cd77
Moore.Huang <moore@moorehy.com>
2022-06-08 22:20:51 +0800

    feat(lspconfig): 配置 jedi_language_server

--- plugins/configs/lspconfig.lua
+++ plugins/configs/lspconfig.lua
@@ -1,17 +1,29 @@
 local M = {}
 
+local merge_tb = vim.tbl_deep_extend
+
 M.setup_lsp = function(attach, capabilities)
-   local lspconfig = require("lspconfig")
+  local lspconfig = require("lspconfig")
 
-   -- lspservers with default config
-   local servers = { "jedi_language_server" }
+  -- lspservers with default config
+  local servers = {
+    ["jedi_language_server"] = {
+      single_file_support = true,
+    },
+  }
 
-   for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
-         on_attach = attach,
-         capabilities = capabilities,
-      }
-   end
+  for server_name, server_opts in pairs(servers) do
+    local opts = {
+      on_attach = attach,
+      capabilities = capabilities,
+      flags = {
+        debounce_text_changes = 150,
+      },
+      settings = {},
+    }
+    opts = merge_tb("force", opts, server_opts)
+    lspconfig[server_name].setup(opts)
+  end
 end
 
 return M
