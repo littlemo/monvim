3d23eb081cec5d0593282dc8f67be68a9759f49e
Moore.Huang <moore@moorehy.com>
2022-07-04 14:12:30 +0800

    chore(chadrc): 调整代码缩进空格数

--- chadrc.lua
+++ chadrc.lua
@@ -6,57 +6,56 @@ local pluginOverride = require("custom.plugins.override")
 local M = {}
 
 M.options = {
+  -- load your options here or load module with options1
+  user = function() end,
 
-   -- load your options here or load module with options1
-   user = function() end,
-
-   nvChad = {
-      -- updater
-      update_url = "https://github.com/NvChad/NvChad",
-      update_branch = "main",
-   },
+  nvChad = {
+    -- updater
+    update_url = "https://github.com/NvChad/NvChad",
+    update_branch = "main",
+  },
 }
 
 ---- UI -----
 
 M.ui = {
-   hl_override = {},
-   changed_themes = {},
-   theme_toggle = { "onedark", "one_light" },
-   theme = "onedark", -- default theme
-   transparency = false,
+  hl_override = {},
+  changed_themes = {},
+  theme_toggle = { "onedark", "one_light" },
+  theme = "onedark", -- default theme
+  transparency = false,
 }
 
 M.plugins = {
    override = {
-     ["nvim-treesitter/nvim-treesitter"] = pluginOverride.treesitter,
-     ["lewis6991/gitsigns.nvim"] = {
-       current_line_blame = true,
-       current_line_blame_opts = {
+    ["nvim-treesitter/nvim-treesitter"] = pluginOverride.treesitter,
+    ["lewis6991/gitsigns.nvim"] = {
+      current_line_blame = true,
+      current_line_blame_opts = {
         delay = 200,
         ignore_whitespace = true,
       },
-     },
-   },
-   remove = {
-     "kyazdani42/nvim-tree.lua",
-     "max397574/better-escape.nvim",
-   },
+    },
+  },
+  remove = {
+    "kyazdani42/nvim-tree.lua",
+    "max397574/better-escape.nvim",
+  },
 
-   options = {
-      lspconfig = {
-         setup_lspconf = "custom.plugins.configs.lspconfig", -- path of lspconfig file
-      },
-      statusline = {
-         separator_style = "default", -- default/round/slant/block/arrow
-      },
-      telescope = {
-         extensions = { "themes", "terms" }
-      }
-   },
+  options = {
+    lspconfig = {
+      setup_lspconf = "custom.plugins.configs.lspconfig", -- path of lspconfig file
+    },
+    statusline = {
+      separator_style = "default", -- default/round/slant/block/arrow
+    },
+    telescope = {
+      extensions = { "themes", "terms" }
+    }
+  },
 
-   -- add, modify, remove plugins
-   user = require("custom.plugins"),
+  -- add, modify, remove plugins
+  user = require("custom.plugins"),
 }
 
 -- check core.mappings for table structure
