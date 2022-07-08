279412a476b49b824f40ddd4cff4ee67f7416eef
Moore.Huang <moore@moorehy.com>
2022-07-08 19:31:55 +0800

    fix(dial): 修复配置不生效问题

--- plugins/configs/dial.lua
+++ plugins/configs/dial.lua
@@ -1,15 +1,14 @@
-local present, dial = pcall(require, "dial")
+local present, augend = pcall(require, "dial.augend")
 
 if not present then
    return
 end
 
-local autocmd = vim.api.nvim_create_autocmd
-local merge_tb = vim.tbl_deep_extend
+-- local autocmd = vim.api.nvim_create_autocmd
+-- local merge_tb = vim.tbl_deep_extend
 local set_keymap = vim.api.nvim_set_keymap
-local buf_set_keymap = vim.api.nvim_buf_set_keymap
+-- local buf_set_keymap = vim.api.nvim_buf_set_keymap
 local keymap_opts = {noremap = true}
-local augend = require("dial.augend")
 local dial_map = require("dial.map")
 
 local group = {
@@ -30,12 +29,12 @@ local group = {
     augend.constant.new{ elements = {"+=", "-="},      word = true, cyclic = true, },
   },
 }
-local extra = {
-  markdown = {
-    augend.misc.alias.markdown_header,
-  },
-}
-group = merge_tb("force", group, extra)
+-- local extra = {
+--   markdown = {
+--     augend.misc.alias.markdown_header,
+--   },
+-- }
+-- group = merge_tb("force", group, extra)
 
 require("dial.config").augends:register_group(group)
 
@@ -46,16 +45,16 @@ set_keymap("v", "<C-x>",  dial_map.dec_visual(),  keymap_opts)
 set_keymap("v", "g<C-a>", dial_map.inc_gvisual(), keymap_opts)
 set_keymap("v", "g<C-x>", dial_map.dec_gvisual(), keymap_opts)
 
-for ft, _ in pairs(extra) do
-  autocmd("FileType", {
-    pattern = ft,
-    callback = function ()
-      buf_set_keymap(0, "n", "<C-a>",  dial_map.inc_normal(ft),  keymap_opts)
-      buf_set_keymap(0, "n", "<C-x>",  dial_map.dec_normal(ft),  keymap_opts)
-      buf_set_keymap(0, "v", "<C-a>",  dial_map.inc_visual(ft),  keymap_opts)
-      buf_set_keymap(0, "v", "<C-x>",  dial_map.dec_visual(ft),  keymap_opts)
-      buf_set_keymap(0, "v", "g<C-a>", dial_map.inc_gvisual(ft), keymap_opts)
-      buf_set_keymap(0, "v", "g<C-x>", dial_map.dec_gvisual(ft), keymap_opts)
-    end
-  })
-end
+-- for ft, _ in pairs(extra) do
+--   autocmd("FileType", {
+--     pattern = ft,
+--     callback = function ()
+--       buf_set_keymap(0, "n", "<C-a>",  dial_map.inc_normal(ft),  keymap_opts)
+--       buf_set_keymap(0, "n", "<C-x>",  dial_map.dec_normal(ft),  keymap_opts)
+--       buf_set_keymap(0, "v", "<C-a>",  dial_map.inc_visual(ft),  keymap_opts)
+--       buf_set_keymap(0, "v", "<C-x>",  dial_map.dec_visual(ft),  keymap_opts)
+--       buf_set_keymap(0, "v", "g<C-a>", dial_map.inc_gvisual(ft), keymap_opts)
+--       buf_set_keymap(0, "v", "g<C-x>", dial_map.dec_gvisual(ft), keymap_opts)
+--     end
+--   })
+-- end
