ba27e50bbee39824e8e1d3f22566000ea4bf5451
Moore.Huang <moore@moorehy.com>
2022-07-08 19:32:19 +0800

    fix(crosshairs): 尝试修复光标行列配色失败问题，未成功

--- init.lua
+++ init.lua
@@ -3,3 +3,9 @@
 -- load your globals, autocmds here or anything .__.
 -- json 格式化
 vim.cmd [[ com! FormatJSON %!python3 -c "import json, sys, collections; print(json.dumps(json.load(sys.stdin, object_pairs_hook=collections.OrderedDict), ensure_ascii=False, indent=4))" ]]
+
+-- 配置光标行列配色
+vim.cmd [[
+  highlight CursorLine   cterm=NONE ctermbg=black ctermfg=NONE guibg=black guifg=NONE
+  highlight CursorColumn cterm=NONE ctermbg=black ctermfg=NONE guibg=black guifg=NONE
+]]
