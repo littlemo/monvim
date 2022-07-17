0fe15922477c565096d4daf20062b13a858ea0ed
Moore.Huang <moore@moorehy.com>
2022-07-17 22:06:36 +0800

    feat(filetype): 按照文件类型设置显示模式

--- init.lua
+++ init.lua
@@ -3,3 +3,12 @@
 -- load your globals, autocmds here or anything .__.
 -- json 格式化
 vim.cmd [[ com! FormatJSON %!python3 -c "import json, sys, collections; print(json.dumps(json.load(sys.stdin, object_pairs_hook=collections.OrderedDict), ensure_ascii=False, indent=4))" ]]
+
+-- 设置文件显示模式
+vim.cmd [[
+  autocmd Filetype python setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr() expandtab tabstop=4 shiftwidth=4 softtabstop=4 nofoldenable
+  autocmd Filetype markdown setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr() noexpandtab tabstop=4 shiftwidth=4 softtabstop=4
+  autocmd Filetype lua setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr() expandtab tabstop=2 shiftwidth=2 softtabstop=2
+  autocmd Filetype json setlocal foldmethod=indent
+  autocmd Filetype yaml setlocal foldmethod=indent expandtab tabstop=2 shiftwidth=2 softtabstop=2 foldlevel=1
+]]
